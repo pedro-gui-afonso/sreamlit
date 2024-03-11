d0f5f130728cdb40cb7cc7a8c93ef57af0795b0c
pedro-gui-afonso <142548349+pedro-gui-afonso@users.noreply.github.com>
2024-03-11 21:01:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 
-page_title = "Expense Tracker App"
+# -------------- SETTINGS --------------
+incomes = ["Salary", "Blog", "Other Income"]
+expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
+currency = "USD"
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "centered"
+# --------------------------------------
 
-st.set_page_config(page_title = page_title, layout=layout)
-st.title(page_title)
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
+
+# --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
+years = [datetime.today().year, datetime.today().year + 1]
+months = list(calendar.month_name[1:])
+
