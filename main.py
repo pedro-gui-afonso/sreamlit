a5380bc1da3a2b716f18c15837778f1479272ec6
pedro-gui-afonso <142548349+pedro-gui-afonso@users.noreply.github.com>
2024-03-11 20:32:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import yfinance as yf
+#import yfinance as yf
 import streamlit as st
 import pandas as pd
 
@@ -13,10 +13,10 @@ Shown are the stock closing price and volume of Google!
 #define the ticker symbol
 tickerSymbol = 'GOOGL'
 #get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
+#tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+#tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+#st.line_chart(tickerDf.Close)
+#st.line_chart(tickerDf.Volume)
