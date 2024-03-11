6f37bae4ae471707c7bf5dd57513ca95b42965bf
pedro-gui-afonso <142548349+pedro-gui-afonso@users.noreply.github.com>
2024-03-11 20:54:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 
-st.write("""
-# Expense Tracker App
-
-Try this absolutely innovative app!
-
-""")
-
 page_title = "Expense Tracker App"
 layout = "centered"
 
